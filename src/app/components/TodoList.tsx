c599ec2f03807e06a1b1a986192d19a3189423a8
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-08-30 12:04:46 +0700

    feat: add server client to request trpc endpoint into SSR

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,16 +1,28 @@
 'use client'
-import React, { useState } from 'react'
 import { trpc } from '../_trpc/client'
-import { number } from 'zod';
+import { serverClient } from '../_trpc/serverClient';
 
 
-function TodoList() {
-    const getTodos = trpc.getTodos.useQuery();
+
+function TodoList({
+    initialTodos
+    }: {
+    initialTodos : Awaited<ReturnType<(typeof serverClient)['getTodos']>> }) {
+    const getTodos = trpc.getTodos.useQuery(undefined,{
+        initialData: initialTodos,
+        refetchOnMount : false,
+        refetchOnReconnect: false
+    });
     const addTodo = trpc.addTodo.useMutation({
         onSettled: () => {
             getTodos.refetch();
         }
     });
+    const reStatus = trpc.reStatus.useMutation({
+        onSettled: () => {
+            getTodos.refetch();
+        }
+    })
 
      function handleAddTodo(event: React.SyntheticEvent<HTMLFormElement>) {
         event.preventDefault()
@@ -27,35 +39,45 @@ function TodoList() {
         event.currentTarget.reset()
     }
 
-    function handleTodoStatus(todoID: number ) {
-        console.log(todoID);
+     function handleTodoStatus(todoID: number,todoStatus: boolean ) {
+        console.log("todoStatus");
+        console.log(todoStatus);
+        
+         reStatus.mutate({
+            id:todoID,
+            isDone:todoStatus
+        })
+        
     }
-    
+
     return (
     <div className='flex flex-col justify-center content-center gap-5'>
-        <div className={"flex flex-col justify-start content-center gap-3"}>
+            <form onSubmit={handleAddTodo}
+                className='flex flex-col justify-center gap-3 p-2 bg-[#183D3D] rounded-md'
+            >
+                <p className='font-extrabold text-2xl'>Add todo</p>
+                <p className='font-semibold'>todo</p>
+                <input name='todo' 
+                    className='rounded-sm'
+                    ></input>
+                <p className='font-semibold'>desciption</p>
+                <input name='desc'
+                    className='rounded-sm'
+                    ></input>
+                <button type='submit'
+                 className=' p-1 bg-[#5C8374] rounded-md hover:bg-[#93B1A6]'    
+                >Add</button>
+            </form>
+        <div className={"flex flex-col-reverse justify-center content-start gap-3 "}>
             {getTodos.data?.map((todo) => 
-            <div>
+            <div className='p-2 bg-[#183D3D] rounded-md'>
                 <div className={"flex gap-2"}>
-                <p>{todo.todo}</p>
-                {todo.isDone ? 
-                <input type='checkbox' onChange={() => handleTodoStatus(todo.id)} checked ></input> :
-                <input type='checkbox' onChange={() => handleTodoStatus(todo.id)}></input>
-            }    
+                <p className='font-semibold text-xl'>{todo.todo}</p>
+                <input type='checkbox' onClick={() => handleTodoStatus(todo.id,(!todo.isDone))} checked={todo.isDone != null ? todo.isDone : false} ></input> 
             </div>
-            <p>{todo.desc}</p>
+            <p className='font-light text-slate-300'>{todo.desc}</p>
             </div>
             )}
-        </div>
-        <div>
-            <form onSubmit={handleAddTodo}>
-                <h3>Add todo</h3>
-                <p>todo</p>
-                <input name='todo' ></input>
-                <p>desciption</p>
-                <input name='desc'></input>
-                <button type='submit'>Add</button>
-            </form>
         </div>
     </div>
   )
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,12 @@
+
 import { todos } from "@/db/schema";
 import { publicProcedure,router } from "./trpc";
 import { drizzle } from "drizzle-orm/mysql2";
+import {eq} from "drizzle-orm"
 import { migrate } from "drizzle-orm/mysql2/migrator";
 import mysql from "mysql2/promise";
 import { z } from "zod";
-
+import { todo } from "@/utils/types";
 
 const connection = await mysql.createConnection({
     host: "127.0.0.1",
@@ -18,8 +20,19 @@ await migrate(db, { migrationsFolder: "drizzle" });
 export const appRouter = router({
     getTodos: publicProcedure
         .query(async () => {
-            const result = await db.select().from(todos)
-            return result
+            // const result  = await db.select().from(todos)
+            const result = z.object({
+                id: z.number(),
+                todo: z.string(),
+                desc: z.string() || z.null(),
+                isDone: z.boolean()
+            }).array()
+            
+
+            const data = await db.select().from(todos)
+            
+            result.parse(data)
+            return data
         }),
     addTodo: publicProcedure
         .input(z.object({
@@ -34,6 +47,25 @@ export const appRouter = router({
                     desc:desc,
                     isDone:false
                 })
+                return true
+            } catch (error) {
+                return false
+            }
+        }),
+    reStatus: publicProcedure
+        .input(z.object({
+            id:z.number(),
+            isDone: z.boolean(),
+        }))
+        .mutation(async (opts) => {
+            const {id,isDone} = opts.input;
+            console.log("isDone");
+            console.log(isDone);
+            
+            try {
+                await db.update(todos)
+                    .set({isDone:isDone})
+                    .where(eq(todos.id,id))
                 return true
             } catch (error) {
                 return false
